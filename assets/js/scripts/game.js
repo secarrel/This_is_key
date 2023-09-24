8b28afb5dd00f1b1aff4ca85aa45370a9eead299
secarrel <secarrel@outlook.com>
2023-09-24 20:38:26 +0100

    Add: function to replace letters in word  for '_'.

--- assets/js/scripts/game.js
+++ assets/js/scripts/game.js
@@ -11,10 +11,11 @@ let keywordOptions = [
 
 // let chosenTopicData = [];
 let keyword = '';
-let tempWordArray = []
+let tempWordArray = [];
 // let maxIncorrect = 8;
 // let incorrectGuesses = 0;
-// let guessedLetters = [];
+let lettersInWord = [];
+let wordStatus = null;
 
 /**
  * Generates a new random word from the 'keywordOptions' array.
@@ -22,5 +23,14 @@ let tempWordArray = []
 function newWord() {
     keyword = keywordOptions[Math.floor(Math.random() * keywordOptions.length)];
     tempWordArray.push(keyword);
+    console.log(tempWordArray);
 }
 
+function underscoreWord() {
+    wordStatus = keyword.split('').map(letter => (lettersInWord.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+    document.getElementById('word-display').innerHTML = wordStatus;
+}
+
+newWord()
+underscoreWord()

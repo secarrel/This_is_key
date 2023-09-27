e34b0414dea93aaac10856fbdcb3c1a1dfae5b80
secarrel <secarrel@outlook.com>
2023-09-27 20:29:48 +0100

    Add: function to create letter array as submitted

--- assets/js/scripts/game.js
+++ assets/js/scripts/game.js
@@ -12,10 +12,11 @@ let keywordOptions = [
 // let chosenTopicData = [];
 let keyword = '';
 let tempWordArray = [];
-// let maxIncorrect = 8;
-// let incorrectGuesses = 0;
+let incorrectGuesses = 0;
 let lettersInWord = [];
-let wordStatus = null;
+let wordProgress = '';
+let possibleLetters = '[ABCDEFGHIJKLMNOPQRSTUVWXYZ]';
+let guessedLetters = [];
 
 /**
  * Generates a new random word from the 'keywordOptions' array.
@@ -23,14 +24,58 @@ let wordStatus = null;
 function newWord() {
     keyword = keywordOptions[Math.floor(Math.random() * keywordOptions.length)];
     tempWordArray.push(keyword);
+    underscoreWord();
     console.log(tempWordArray);
+    console.log(keywordOptions);
 }
 
+/**
+ * Replaces all letters in the keyword with underscores.
+ */
 function underscoreWord() {
-    wordStatus = keyword.split('').map(letter => (lettersInWord.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+    wordProgress = keyword.split('').map(letter => (lettersInWord.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
-    document.getElementById('word-display').innerHTML = wordStatus;
+    document.getElementById('word-display').innerHTML = wordProgress;
 }
 
-newWord()
 underscoreWord()
+
+document.getElementById("letter-input").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("letter-input").value = "";
+        document.getElementById("letter-input").focus();    
+    }
+})
+
+function checkIfLetter(event) {
+    let guess = event.key;
+    let upperGuess = guess.toUpperCase();
+    let validGuess = possibleLetters.indexOf(upperGuess) !== -1;
+    
+    if (validGuess == true) {
+        guessedLetters.push(upperGuess);
+    }
+    
+    console.log(upperGuess);
+    console.log(validGuess);
+    console.log(guessedLetters)
+
+    // checkIfDuplicate();
+}
+
+// function checkIfDuplicate(upperGuess, guessedLetters){
+//     for (let i = 0; i < guessedLetters.length; i++) {
+//         if (guessedLetters[i] === upperGuess) {
+//             guessedLetters.pop(upperGuess);
+//             alert("You have already guessed this letter")
+//         } 
+//     }
+// }
+
+// function submitLetter(event) {
+//     checkIfDuplicate()
+// }
+
+function check() {
+    
+}

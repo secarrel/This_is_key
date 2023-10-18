ead34671492639bb0c318891a44098a244c5a60f
secarrel <secarrel@outlook.com>
2023-10-18 13:34:16 +0100

    Test: current score functionality

--- assets/js/scripts/tests/game.test.js
+++ assets/js/scripts/tests/game.test.js
@@ -1,19 +1,33 @@
-/**
- * @jest-environment jsdom
- */
-
-const {currentScore} = require("./game.test.js");
+const { displayScore, checkForWin, checkForLoss } = require("../game");
 
 beforeAll(() => {
-    let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
+  const fs = require("fs");
+  const fileContents = fs.readFileSync("index.html", "utf-8");
+  document.open();
+  document.write(fileContents);
+  document.close();
 });
 
-describe("game display contains correct keys", () => {
-    test("score key exists", () => {
-        expect(currentScore).toEqual(0);
-    });
+describe("current score is functional and equal to the number of wins", () => {
+  test("current score exists", () => {
+    expect(displayScore()).toBeDefined();
+  });
+  test("current score equals 0 at start of game", () => {
+    expect(displayScore()).toEqual(0);
+  });
+  beforeAll(() => {
+    win = true;
+  });
+  test("Current score increases if a win is achieved", () => {
+    expect(checkForWin()).toEqual(1);
+  });
 });
+
+// describe("game display contains correct elements", () => {
+//   test("current score exists", () => {
+//     expect(displayScore()).toBeGreaterThanOrEqual(0);
+//   });
+//   test("Current topic display exists", () => {
+//     expect()
+//   })
+// });
